6b1844292b84edcb8213048c74146903bff42aae
Marcos Rosales <marcostadeorosales@gmail.com>
2023-05-02 20:14:39 -0300

    Commit Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculadoraComponent } from './components/calculadora/calculadora.component';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculadoraComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, InputNumberModule,FormsModule,ReactiveFormsModule,ButtonModule,CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/calculadora/calculadora.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrls: ['./calculadora.component.css']
+})
+export class CalculadoraComponent {
+  tna: number = 91
+  meses!: number
+  sueldo!: number
+  dineroAcumulado:number = 0
+  totalGanancias:number = 0
+  gananciasPorMes:number = 0
+  mostrarResultado: boolean = false
+
+  dineroAcumuladoString!:string
+  totalGananciasString!:string
+  gananciasPorMesString!:string
+  constructor(){}
+
+  calcular(){
+    this.dineroAcumulado = 0;
+    this.totalGanancias = 0;
+    this.gananciasPorMes = 0;
+
+    var tna = this.tna/12
+    tna = tna/100    
+    this.dineroAcumulado = 0
+    for(var i = 1; i<=this.meses; i++){
+      this.gananciasPorMes = (this.dineroAcumulado+this.sueldo)*tna
+      this.dineroAcumulado = this.dineroAcumulado+ this.sueldo + this.gananciasPorMes
+      this.totalGanancias = this.totalGanancias + this.gananciasPorMes
+    }
+    this.totalGanancias = Number(parseFloat(this.totalGanancias.toString()).toFixed(2))
+    this.dineroAcumulado = Number(parseFloat(this.dineroAcumulado.toString()).toFixed(2))
+    this.gananciasPorMes = Number(parseFloat(this.gananciasPorMes.toString()).toFixed(2))
+    this.mostrarResultado = true
+
+    this.totalGananciasString = this.totalGanancias.toString().replace('.', ',')
+    this.dineroAcumuladoString = this.dineroAcumulado.toString().replace('.', ',')
+    this.gananciasPorMesString = this.gananciasPorMes.toString().replace('.', ',')
+    this.totalGananciasString = this.totalGananciasString.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    this.dineroAcumuladoString = this.dineroAcumuladoString.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    this.gananciasPorMesString = this.gananciasPorMesString.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  }
+}
